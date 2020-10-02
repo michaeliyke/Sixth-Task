8112e0d00abe08b59d289aa2e4f12f504ab06722
Michael C. Iyke <ymichaelc@gmail.com>
2020-10-02 18:40:10 +0100

    GET front-end request has been partially achieved

--- michaeliyke/public/js/index.js
+++ michaeliyke/public/js/index.js
@@ -2,6 +2,10 @@
    jQuery((function(w, sapi){
        return function($){
         const a = alert;
+
+        const ajaxRequest = new XMLHttpRequest();
+
+
          $(sapi.forms).change(function(event) {
            if(event.target.classList.contains("form-check-input")) {
             $(this).find(".input-field").each(function() {
@@ -9,8 +13,31 @@
             });
           }
 
-          $(".show-room").text(this.method.toUpperCase());
+          ajaxRequest.onreadystatechange = function(event) {
+              console.log(ajaxRequest.responseText)
+            if (ajaxRequest.status == 200) {
+              $(".show-room").text(ajaxRequest.responseText);
+
+            }
+          };
+          ajaxRequest.open("GET", "/");
+          ajaxRequest.send();
+            // const method = this.dataset.method.toUpperCase();
             
          });
+
+
+
+
+       function queryServer(method, fn, options) {
+         ajaxRequest.addEventListener("error", (error) => {
+           return a(error);
+         });
+         ajaxRequest.addEventListener("load", (event) => {
+           fn.call(null, event.response);
+         });
+         ajaxRequest.open(method, options.url || "127.0.0.1:3000", false );
+         ajaxRequest.send();
+       }  
        }
     }(window, document)));
